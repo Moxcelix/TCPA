03c4962f5b43fe66b4bed2212633be5cae67ae14
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-05-31 20:26:59 +0300

    Create GeneralPurposeRegisters.cs

---
+++ Application/GeneralPurposeRegisters.cs
@@ -0,0 +1,34 @@
+﻿using TCPA.Models;
+
+namespace TCPA.Application
+{
+    internal class GeneralPurposeRegisters : IRegisterBlock
+    {
+        private readonly byte[] _registers = new byte[16];
+
+        public byte DataBus { get; set; }
+        public byte CodeBus { get; set; }
+
+        public void Update()
+        {
+            if (!CheckBit(CodeBus, 7))
+            {
+                return;
+            }
+
+            if(CheckBit(CodeBus, 6))
+            {
+                _registers[CodeBus & 0b_0000_1111] = DataBus;
+            }
+            else
+            {
+                DataBus = _registers[CodeBus & 0b_0000_1111];
+            }
+        }
+
+        private static bool CheckBit(byte data, byte position)
+        {
+            return (data & (0x1 << position)) != 0;
+        }
+    }
+}
