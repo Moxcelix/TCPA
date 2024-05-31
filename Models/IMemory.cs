92605bdf5c83b035459cba421d87fb7a06747d53
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-05-31 17:46:46 +0300

    Create IMemory.cs

---
+++ Models/IMemory.cs
@@ -0,0 +1,15 @@
+﻿namespace TCPA.Models
+{
+    internal interface IMemory
+    {
+        public byte DataBus {  get; set; }
+
+        public byte AddressBus {  get; set; }
+
+        public bool Enabled {  get; set; }
+
+        public bool RWMode { get; set; }
+
+        public bool Ready { get; }
+    }
+}
