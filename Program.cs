cb2bf84e1a44964617613e7a699bcf30769c7f03
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-05-31 22:29:32 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TCPA.Infrastructure;
+
+var alu = new ArithmeticLogicUnit();
+alu.DataBus = 255;
+alu.CodeBus = 0b_1001_0000;
+alu.Update();
+alu.DataBus = 0;
+alu.CodeBus = 0b_1010_0000;
+alu.Update();
+alu.CodeBus = 0b_1011_0000;
+do
+{
+    alu.Update();
+} while (!alu.Ready);
+
+Console.WriteLine(alu.DataBus);
+Console.WriteLine(alu.C);
