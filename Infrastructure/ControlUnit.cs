45e2a87d9ff17bfe16c4423530ae567f6cca6f0e
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-05-31 22:42:56 +0300

    Update ControlUnit.cs

--- Infrastructure/ControlUnit.cs
+++ Infrastructure/ControlUnit.cs
@@ -1,6 +1,23 @@
-﻿namespace TCPA.Infrastructure
+﻿using TCPA.Models;
+
+namespace TCPA.Infrastructure
 {
-    internal class ControlUnit
+    internal class ControlUnit : IControlUnit
     {
+        public byte DataBus { get; set; }
+        public byte AddressBus { get; set; }
+        public bool Reset { get; set; }
+        public bool Enabled { get; set; }
+
+        public byte RegisterCodeBus => throw new NotImplementedException();
+
+        public byte ALUCodeBus => throw new NotImplementedException();
+
+        public byte MemoryCodeBus => throw new NotImplementedException();
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
