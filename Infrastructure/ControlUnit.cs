b2a8bb449caf42fb07491e7b74f3827e67cc0672
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-06-02 02:52:10 +0300

    Update ControlUnit.cs

--- Infrastructure/ControlUnit.cs
+++ Infrastructure/ControlUnit.cs
@@ -9,17 +9,22 @@ namespace TCPA.Infrastructure
             DISABLED,
             REQUEST_READ_BYTE,
             READ_BYTE,
-            READ_OP0
+            CHECK_PART,
+            CHECK_DOUBLE_OP,
+            CHECK_MOV,
+            NEXT_CC_ALU_FIRST,
+            NEXT_CC_ALU_SECOND,
+            NEXT_CC_ALU_RESULT,
         }
 
         private const byte READ_MEMORY      = 0b_1000_0000;
         private const byte WRITE_MEMORY     = 0b_1000_0001;
         private const byte DISABLE_MEMORY   = 0b_0000_0000;
 
-        private const byte ALU_FIRST = 0b_1001_0000;
-        private const byte ALU_SECOND = 0b_1010_0000;
-        private const byte ALU_RESULT = 0b_1011_0000;
-        private const byte ALU_DISABLE = 0b_0000_0000;
+        private const byte ALU_FIRST    = 0b_1001_0000;
+        private const byte ALU_SECOND   = 0b_1010_0000;
+        private const byte ALU_RESULT   = 0b_1011_0000;
+        private const byte ALU_DISABLE  = 0b_0000_0000;
 
         private State _state = State.DISABLED;
         private byte _cmd = 0;
@@ -28,6 +33,7 @@ namespace TCPA.Infrastructure
         private byte _acc = 0;
         private byte _buf = 0;
         private byte _cc = 0;
+        private byte _pc = 0;
 
         public byte DataBus { get; set; }
 
@@ -67,11 +73,46 @@ namespace TCPA.Infrastructure
                 case State.READ_BYTE:
                     if (MemoryReady)
                     {
-                        _cmd = DataBus;
                         MemoryCodeBus = DISABLE_MEMORY;
-                        DataBus = _cc;
-                        ALUCodeBus = ALU_FIRST;
-                        _state = State.READ_BYTE;
+                        _state = State.CHECK_PART;
+                    }
+                    break;
+                case State.CHECK_PART:
+                    if (_pc == 0)
+                    {
+                        _cmd = DataBus;
+                        _pc = 1;
+                        _state = State.NEXT_CC_ALU_FIRST;
+                    }
+                    else if(_pc == 1)
+                    {
+                        _pc = 2;
+                        _op0 = DataBus;
+                        _state = State.CHECK_DOUBLE_OP;
+                    }
+                    else if (_pc == 2)
+                    {
+                        _pc = 3;
+                        _op1 = DataBus;
+                        _state = State.CHECK_MOV;
+                    }
+                    break;
+                case State.NEXT_CC_ALU_FIRST:
+                    DataBus = _cc;
+                    ALUCodeBus = ALU_FIRST;
+                    _state = State.NEXT_CC_ALU_SECOND;
+                    break;
+                case State.NEXT_CC_ALU_SECOND:
+                    DataBus = 1;
+                    ALUCodeBus = ALU_SECOND;
+                    _state = State.NEXT_CC_ALU_RESULT;
+                    break;
+                case State.NEXT_CC_ALU_RESULT:
+                    if(ALUReady)
+                    {
+                        _cc = DataBus;
+                        ALUCodeBus = ALU_DISABLE;
+                        _state = State.REQUEST_READ_BYTE;
                     }
                     break;
             }
