0d415fea56426f63bd9da514c178bf62e108c77e
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-06-01 23:26:06 +0300

    Update ControlUnit.cs

--- Infrastructure/ControlUnit.cs
+++ Infrastructure/ControlUnit.cs
@@ -7,8 +7,27 @@ namespace TCPA.Infrastructure
         private enum State
         {
             DISABLED,
-            READ_INSTURCTION
+            REQUEST_READ_BYTE,
+            READ_BYTE,
+            READ_OP0
         }
+
+        private const byte READ_MEMORY      = 0b_1000_0000;
+        private const byte WRITE_MEMORY     = 0b_1000_0001;
+        private const byte DISABLE_MEMORY   = 0b_0000_0000;
+
+        private const byte ALU_FIRST = 0b_1001_0000;
+        private const byte ALU_SECOND = 0b_1010_0000;
+        private const byte ALU_RESULT = 0b_1011_0000;
+        private const byte ALU_DISABLE = 0b_0000_0000;
+
+        private State _state = State.DISABLED;
+        private byte _cmd = 0;
+        private byte _op0 = 0;
+        private byte _op1 = 0;
+        private byte _acc = 0;
+        private byte _buf = 0;
+        private byte _cc = 0;
 
         public byte DataBus { get; set; }
 
@@ -16,9 +35,9 @@ namespace TCPA.Infrastructure
 
         public bool Enabled { get; set; }
 
-        public byte ALUReady { get; set; }
+        public bool ALUReady { get; set; }
 
-        public byte MemoryReady { get; set; }
+        public bool MemoryReady { get; set; }
 
         public byte AddressBus { get; private set; }
 
@@ -30,7 +49,32 @@ namespace TCPA.Infrastructure
 
         public void Update()
         {
+            if (!Enabled)
+            {
+                return;
+            }
 
+            switch (_state)
+            {
+                case State.DISABLED:
+                    _state = State.REQUEST_READ_BYTE;
+                    break;
+                case State.REQUEST_READ_BYTE:
+                    AddressBus = _cc;
+                    MemoryCodeBus = READ_MEMORY;
+                    _state = State.READ_BYTE;
+                    break;
+                case State.READ_BYTE:
+                    if (MemoryReady)
+                    {
+                        _cmd = DataBus;
+                        MemoryCodeBus = DISABLE_MEMORY;
+                        DataBus = _cc;
+                        ALUCodeBus = ALU_FIRST;
+                        _state = State.READ_BYTE;
+                    }
+                    break;
+            }
         }
     }
 }
