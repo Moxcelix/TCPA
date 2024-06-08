9d5e0ae6a169c4145cb0f03251ed726bd9c001d1
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2024-06-08 19:05:59 +0300

    some pres

--- Application/Application.cs
+++ Application/Application.cs
@@ -1,7 +1,44 @@
-﻿namespace TCPA.Application
+﻿using TCPA.Infrastructure;
+
+namespace TCPA.Application
 {
-    internal class Application(Controller controller)
+    internal class Application
     {
-        private readonly Controller _controller = controller;
+        private readonly Controller _controller;
+        private readonly LinearMemory _memory;
+        private readonly ArithmeticLogicUnit _alu;
+        private readonly GeneralPurposeRegisters _registers;
+        private readonly ControlUnit _controlUnit;
+
+        internal static readonly char[] separator = [' ', '\r', '\n'];
+
+        public Application()
+        {
+            _memory = new LinearMemory(64);
+            _alu = new ArithmeticLogicUnit();
+            _registers = new GeneralPurposeRegisters();
+            _controlUnit = new ControlUnit();
+
+            _controller = new Controller(
+                _memory,
+                _alu, 
+                _registers, 
+                _controlUnit);
+        }
+
+        public void LoadData(string path)
+        {
+            if(File.Exists(path))
+            {
+                var file = File.ReadAllText(path);
+
+                byte[] byteArray = file
+                   .Split(separator, StringSplitOptions.RemoveEmptyEntries)
+                   .Select(hex => Convert.ToByte(hex, 16))
+                   .ToArray();
+
+                _memory.SetData(byteArray);
+            }
+        }
     }
 }
